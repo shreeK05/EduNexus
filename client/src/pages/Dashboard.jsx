54d872051081d61aa75ca4ff3571751452e9dba7
shreeK05 <shree.k1510@gmail.com>
2026-02-11 00:29:46 +0530

    Fixed typo in Join Class URL

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -68,10 +68,10 @@ const Dashboard = () => {
     e.preventDefault();
     try {
       if (user.role === 'TEACHER') {
-        await axios.post('https://https://edunexus-api-o8qg.onrender.com/api/classes/create', { ...formData, teacherId: user._id });
+        await axios.post('https://edunexus-api-o8qg.onrender.com/api/classes/create', { ...formData, teacherId: user._id });
         alert('Classroom Created Successfully!');
       } else {
-        await axios.post('https://https://edunexus-api-o8qg.onrender.com/api/classes/join', { code: formData.code, studentId: user._id });
+        await axios.post('https://edunexus-api-o8qg.onrender.com/api/classes/join', { code: formData.code, studentId: user._id });
         alert('Joined Class Successfully!');
       }
       setShowModal(false);
