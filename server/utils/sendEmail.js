47e219f3c63d6a04a9e68f5c59f8399d96b79b25
shreeK05 <shree.k1510@gmail.com>
2026-04-30 18:08:08 +0530

    Fix: Add support for verified EMAIL_FROM sender

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -33,7 +33,7 @@ const sendEmail = async (options) => {
     });
 
     const mailOptions = {
-      from: `"EduNexus LMS" <${process.env.EMAIL_USER}>`,
+      from: `"EduNexus Support" <${process.env.EMAIL_FROM || process.env.EMAIL_USER}>`,
       to: options.email,
       subject: options.subject,
       html: options.message,
